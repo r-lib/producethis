8286b8a6299495ccf91fc2b84fade372185894f0
Thomas Lin Pedersen <thomasp85@gmail.com>
2024-12-05 15:09:32 +0100

    fix typing error in rprofile setup

--- R/prepare_rprofile.R
+++ R/prepare_rprofile.R
@@ -3,8 +3,8 @@ prepare_rprofile <- function() {
   if (!fs::file_exists(file)) {
     fs::file_create(file)
   }
-  cat('proj_opts <- eval(parse_expr(read.dcf("DESCRIPTION", "Settings/R")))\n', file = file, append = TRUE)
-  cat('if (is.list(proj_opts) options(proj_opts)\n', file = file, append = TRUE)
+  cat('proj_opts <- eval(parse(text = read.dcf("DESCRIPTION", "Settings/R")))\n', file = file, append = TRUE)
+  cat('if (is.list(proj_opts)) options(proj_opts)\n', file = file, append = TRUE)
 
   cat("\n\npkgload::load_all()\n", file = file, append = TRUE)
 }
